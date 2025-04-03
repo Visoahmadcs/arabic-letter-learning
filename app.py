538db67084f5b9619e504890df6990b57619e991
Visoahmadcs <ahmadishaqcs@gmail.com>
2025-04-03 17:41:55 +0900

    Add home page and GitHub Pages support

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/lesson/<int:lesson_id>')
+def lesson(lesson_id):
+    return render_template('lesson.html', lesson_id=lesson_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
